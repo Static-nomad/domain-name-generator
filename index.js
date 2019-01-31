fec42187c4f15a97046598a24796000dbc5b56c2
Melanie Young <ms.melanieyoung@gmail.com>
2019-01-31 18:56:50 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var pronoun = ['The','Your'];
+var adj = ['little', 'crazy' ];
+var noun = ['donkey','house'];
+
+   
+let generateDomain = () => {   
+var combos = [] ;
+
+for(var i = 0; i < pronoun.length; i++){
+    for(var j = 0; j < adj.length; j++){
+        for(var k = 0; k < noun.length; k++){
+            combos.push(pronoun[i] + adj[j] + noun[k] + ".com");
+        }
+     }
+}
+
+document.getElementById("domain").innerHTML = combos;
+
+};
+console.log(generateDomain(document.getElementById("domain").innerHTML));
